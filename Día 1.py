115ed1ad0931bdab5d97524fb78c1ee84fbe1ce3
JorgeSTEM <145747302+JorgeSTEM@users.noreply.github.com>
2023-09-22 11:08:57 +0200

    Add files via upload
    Día 1 Python

---
+++ Día 1.py
@@ -0,0 +1,33 @@
+print("Hola Mundo")
+print ("Me llamo Jorge")
+print ("Jorge")
+#Que bonito es Python
+#Nuestra primera variable
+x=5
+print(5+5)#imprime 10
+print (5) #imprime 5
+print (x)
+print ("print (5):")
+print ("print (5+5):")
+print ("print (x):") #imprime 5
+print (x+10)
+#Creamos una variable con texto -> string
+nombre= "jorge"
+numero= 8
+print (nombre)
+print ("sumo nombre+numero:")
+print ("Jorge"+"5")
+print (nombre+str(numero))
+print ('print("Jorge"+"8")')
+print ('print(nombre+str(numero)):')
+print ("cucu"*10)
+print ("Escribo 10 veces cucu")
+print ()
+print ("\n\Hacemos \t\ \"operaciones\" \t\matemáticas")
+num1=2
+num2=8
+suma=num1+num2
+resta=num1-num2
+print=("suma="+str(suma)+"|resta="+str(resta) +"\ |multiplicacion= " +str (multp)+ "\ | div= "+str(div))
+multp=num1*num2
+div=num1/num2
